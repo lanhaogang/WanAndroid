965321937b9242813974839c29525d6df4ab7d24
lanhaogang <497444760@qq.com>
2019-01-06 16:03:58 +0800

    TPWanAndroid: Add basic dependencies and views for MainActivity
    描述：添加基本的MeterialDesign依赖和相关组件

--- app/src/main/java/com/peng/wanandroid/MainActivity.java
+++ app/src/main/java/com/peng/wanandroid/MainActivity.java
@@ -3,10 +3,26 @@ package com.peng.wanandroid;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.peng.wanandroid.base.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
     }
 }
